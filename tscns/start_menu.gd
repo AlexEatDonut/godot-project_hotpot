4548efb6f56ec53b34209118df64b28a9194ad2d
AlexEatDonut <alexeatdonut@gmail.com>
2024-11-18 15:13:25 +0100

    updated main menu

--- tscns/start_menu.gd
+++ tscns/start_menu.gd
@@ -15,3 +15,9 @@ func _on_btnarena_pressed() -> void:
 	SceneTransitionAnimation.play("fadeIn")
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://tscns/level_openarena.tscn")
+
+
+func _on_btnarenabasic_pressed() -> void:
+		SceneTransitionAnimation.play("fadeIn")
+		await get_tree().create_timer(0.5).timeout
+		get_tree().change_scene_to_file("res://tscns/level_standardArena.tscn")
