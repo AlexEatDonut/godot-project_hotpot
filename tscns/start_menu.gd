f2dc97c00c78fa0fec0174567a68423129febaeb
AlexEatDonut <alexeatdonut@gmail.com>
2024-11-20 16:46:14 +0100

    updated main menu with choice text and sfx

--- tscns/start_menu.gd
+++ tscns/start_menu.gd
@@ -5,19 +5,26 @@ extends CanvasLayer
 func gameStarted():
 	pass
 
+func button_click_sfx():
+	var btn_onclick_sfx = preload("res://sounds/ui/button_onclick.tscn").instantiate()
+	get_tree().current_scene.add_child(btn_onclick_sfx)
+
 func _on_btnsurfing_pressed() -> void:
+	button_click_sfx()
 	SceneTransitionAnimation.play("fadeIn")
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://tscns/level.tscn")
 
 
 func _on_btnarena_pressed() -> void:
+	button_click_sfx()
 	SceneTransitionAnimation.play("fadeIn")
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://tscns/level_openarena.tscn")
 
 
 func _on_btnarenabasic_pressed() -> void:
-		SceneTransitionAnimation.play("fadeIn")
-		await get_tree().create_timer(0.5).timeout
-		get_tree().change_scene_to_file("res://tscns/level_standardArena.tscn")
+	button_click_sfx()
+	SceneTransitionAnimation.play("fadeIn")
+	await get_tree().create_timer(0.5).timeout
+	get_tree().change_scene_to_file("res://tscns/level_standardArena.tscn")
